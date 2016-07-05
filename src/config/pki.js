c1e7f28971c71cd3db9786029ffec070a6b05c90
detailyang <detailyang@gmail.com>
2016-07-05 13:07:10 +0800

    update gitignore

--- src/config/pki.js
+++ src/config/pki.js
@@ -3,7 +3,7 @@
 * @Date:   2016-03-16T22:03:58+08:00
 * @Email:  detailyang@gmail.com
 * @Last modified by:   detailyang
-* @Last modified time: 2016-07-02T21:44:33+08:00
+* @Last modified time: 2016-07-05T13:06:27+08:00
 * @License: The MIT License (MIT)
 */
 
@@ -11,22 +11,21 @@
 import path from 'path';
 
 
+const pkidir = process.env.CAS_PKI_DIR || path.join(
+  path.dirname(path.dirname(path.dirname(__filename))), 'pki');
+
 if (process.env.NODE_ENV === 'dev') {
   module.exports = {
-    dir: path.join(path.dirname(path.dirname(path.dirname(__filename))), 'pki'),
+    dir: pkidir,
     days: process.env.CAS_PKI_DAYS || 30,
     password: process.env.CAS_PKI_PASSWORD || 'password',
     subj: process.env.CAS_PKI_SUBJ || '/C=US/ST=NY/L=New York/O=CAS',
     ca: {
-      x509: process.env.CAS_PKI_CA_X509
-        || path.join(path.dirname(path.dirname(path.dirname(__filename))), 'pki/x509.cnf.dev'),
       passin: process.env.CAS_PKI_CA_PASSIN || '1234',
-      unsecurekey: process.env.CAS_PKI_CA_KEY || path.join(path.dirname(path.dirname(
-        path.dirname(__filename))), 'pki/unsecure.ca.key'),
-      key: process.env.CAS_PKI_CA_KEY || path.join(path.dirname(path.dirname(
-        path.dirname(__filename))), 'pki/ca.key'),
-      crt: process.env.CAS_PKI_CA_CRT || path.join(path.dirname(path.dirname(
-        path.dirname(__filename))), 'pki/ca.crt'),
+      x509: process.env.CAS_PKI_CA_X509 || path.join(pkidir, 'x509.cnf.dev'),
+      unsecurekey: process.env.CAS_PKI_CA_KEY || path.join(pkidir, 'unsecure.ca.key'),
+      key: process.env.CAS_PKI_CA_KEY || path.join(pkidir, 'ca.key'),
+      crt: process.env.CAS_PKI_CA_CRT || path.join(pkidir, 'ca.crt'),
     },
   };
 } else if (process.env.NODE_ENV === 'test') {
@@ -36,15 +35,11 @@ if (process.env.NODE_ENV === 'dev') {
     password: process.env.CAS_PKI_PASSWORD || 'password',
     subj: process.env.CAS_PKI_SUBJ || '/C=US/ST=NY/L=New York/O=CAS',
     ca: {
-      x509: process.env.CAS_PKI_CA_X509
-        || path.join(path.dirname(path.dirname(path.dirname(__filename))), 'pki/x509.cnf.test'),
       passin: process.env.CAS_PKI_CA_PASSIN || '1234',
-      unsecurekey: process.env.CAS_PKI_CA_KEY || path.join(path.dirname(path.dirname(
-        path.dirname(__filename))), 'pki/unsecure.ca.key'),
-      key: process.env.CAS_PKI_CA_KEY || path.join(path.dirname(path.dirname(
-        path.dirname(__filename))), 'pki/ca.key'),
-      crt: process.env.CAS_PKI_CA_CRT || path.join(path.dirname(path.dirname(
-        path.dirname(__filename))), 'pki/ca.crt'),
+      x509: process.env.CAS_PKI_CA_X509 || path.join(pkidir, 'x509.cnf.dev'),
+      unsecurekey: process.env.CAS_PKI_CA_KEY || path.join(pkidir, 'unsecure.ca.key'),
+      key: process.env.CAS_PKI_CA_KEY || path.join(pkidir, 'ca.key'),
+      crt: process.env.CAS_PKI_CA_CRT || path.join(pkidir, 'ca.crt'),
     },
   };
 } else {
@@ -54,15 +49,11 @@ if (process.env.NODE_ENV === 'dev') {
     password: process.env.CAS_PKI_PASSWORD || 'password',
     subj: process.env.CAS_PKI_SUBJ || '/C=US/ST=NY/L=New York/O=CAS',
     ca: {
-      x509: process.env.CAS_PKI_CA_X509
-        || path.join(path.dirname(path.dirname(path.dirname(__filename))), 'pki/x509.cnf.prod'),
       passin: process.env.CAS_PKI_CA_PASSIN || '1234',
-      unsecurekey: process.env.CAS_PKI_CA_KEY || path.join(path.dirname(path.dirname(
-        path.dirname(__filename))), 'pki/unsecure.ca.key'),
-      key: process.env.CAS_PKI_CA_KEY || path.join(path.dirname(path.dirname(
-        path.dirname(__filename))), 'pki/ca.key'),
-      crt: process.env.CAS_PKI_CA_CRT || path.join(path.dirname(path.dirname(
-        path.dirname(__filename))), 'pki/ca.crt'),
+      x509: process.env.CAS_PKI_CA_X509 || path.join(pkidir, 'x509.cnf.dev'),
+      unsecurekey: process.env.CAS_PKI_CA_KEY || path.join(pkidir, 'unsecure.ca.key'),
+      key: process.env.CAS_PKI_CA_KEY || path.join(pkidir, 'ca.key'),
+      crt: process.env.CAS_PKI_CA_CRT || path.join(pkidir, 'ca.crt'),
     },
   };
 }
