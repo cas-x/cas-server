f1681441ce45f9bd0249f5676ea270f8c4859674
detailyang <detailyang@gmail.com>
2016-07-14 14:11:54 +0800

    delete api/pkis/server

--- src/routes/api/pki.js
+++ src/routes/api/pki.js
@@ -2,11 +2,10 @@
  * @Author: detailyang
  * @Date:   2016-02-18 12:43:02
 * @Last modified by:   detailyang
-* @Last modified time: 2016-07-14T14:08:18+08:00
+* @Last modified time: 2016-07-14T14:10:08+08:00
  */
 import koarouter from 'koa-router';
 import controllers from '../../controllers';
-import utils from '../../utils';
 
 
 const router = koarouter({
@@ -14,12 +13,6 @@ const router = koarouter({
 });
 module.exports = router;
 
-const adminOnly = async (ctx, next) => {
-  if (!ctx.session.is_admin) {
-    throw new utils.error.PermissionError('you are not admin');
-  }
-  await next();
-};
 
 router.get('/ca', controllers.pki.ca.get);
 router.get('/client', controllers.pki.client.getByUid);
